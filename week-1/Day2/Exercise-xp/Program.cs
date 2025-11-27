f1eadde6aa2bb684cd6b35686ce71992ec57b22e
Abdo <31053920+AbdoMusk@users.noreply.github.com>
2025-11-27 17:57:26 +0100

    Add files via upload

---
+++ week-1/Day2/Exercise-xp/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class GameApp
+{
+    static void Main()
+    {
+        Console.WriteLine("Game Simulation Started!");
+        Console.WriteLine("Loading...");
+        Console.WriteLine("Welcome to the Game Arena!");
+    }
+}
